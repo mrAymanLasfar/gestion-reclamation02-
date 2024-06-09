847de7692cfc03e36e6a63e1595a0895a3dbbafe
Ayman_GIT <02012004.al@gmail.com>
2024-06-09 18:48:23 +0100

    commi num2 09/06/2024

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Role extends Model
 {
     use HasFactory;
+    public function users()
+    {
+        return $this->morphedByMany(User::class, 'model', 'model_has_roles', 'role_id', 'model_id');
+    }
 }
