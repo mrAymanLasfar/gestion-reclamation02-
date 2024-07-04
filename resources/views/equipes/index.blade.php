a0b5815112b681e770ec2240b63852cce5a3dbd4
yassineboubkeur <boubkeuryassine2@gmail.com>
2024-07-04 13:59:09 +0100

    commit 04/07/2024

--- resources/views/equipes/index.blade.php
+++ resources/views/equipes/index.blade.php
@@ -10,8 +10,36 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div>
-                        <a href="{{ route('creerequipe') }}" class="btn btn-primary">Voir les Équipes</a> <br>
-                        <a href="{{ route('creercoordinateur') }}" class="btn btn-primary">Voir les Coordinateurs</a>
+                    
+                        <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                            <div class="grid grid-cols-1 md:grid-cols-2">
+                                <div class="p-6">
+                                    <div class="flex items-center">
+                                        <svg class="w-8 h-8 text-gray-500 " fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path d="M17 20h5v-2a3 3 0 00-5.89-1H5.89A3 3 0 000 18v2h5v-2a3 3 0 015.89-1h4.22A3 3 0 0017 20zM7 14a3 3 0 100-6 3 3 0 000 6zm10-3a3 3 0 110-6 3 3 0 010 6z"></path>
+                                        </svg>
+        
+                                        </svg>
+                                        <div class="ml-4 text-lg leading-7 font-semibold">
+                                            <a href="{{ route('creerequipe') }}" class="underline text-gray-900 dark:text-dark btn btn-dark">Voir les Équipes</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+                                    <div class="flex items-center">
+                                        <svg class="w-8 h-8 text-gray-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path d="M9.75 9a4.25 4.25 0 108.5 0 4.25 4.25 0 00-8.5 0zM19.25 22.5v-2.875a2.875 2.875 0 00-2.875-2.875h-8.75A2.875 2.875 0 004.75 22.5V24h14.5v-1.5z"></path>
+
+                                        </svg>
+                                        <div class="ml-4 text-lg leading-7 font-semibold">
+                                            <a href="{{ route('creercoordinateur') }}" class="underline text-gray-900 dark:text-dark btn btn-dark">Voir les Coordinateurs</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                          
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
